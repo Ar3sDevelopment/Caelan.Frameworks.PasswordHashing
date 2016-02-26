24506381c8f40ed0d5074ab49b62c41263845fdf
Matteo Bruni <ar3s@icloud.com>
2016-02-26 12:46:47 +0100

    startup

---
+++ Caelan.Frameworks.PasswordHashing.NUnit/PasswordTest.fs
@@ -0,0 +1,18 @@
+﻿namespace Caelan.Frameworks.PasswordHashing.NUnit
+
+open System
+open NUnit.Framework
+open Caelan.Frameworks.PasswordHashing.Classes
+
+type CustomPasswordHasher() = 
+    inherit PasswordHasher("salt", "default")
+
+[<TestFixture>]
+type PasswordTest() = 
+    [<Test>]
+    member __.TestHasher() = 
+        let pwd = CustomPasswordHasher()
+        "password"
+        |> pwd.HashPassword
+        |> printfn "%s"
+        pwd.DefaultPasswordHashed |> printfn "%s"
---
+++ Caelan.Frameworks.PasswordHashing/IPasswordHasher.fs
@@ -0,0 +1,8 @@
+﻿namespace Caelan.Frameworks.PasswordHashing.Interfaces
+
+type IPasswordHasher = 
+    /// <summary>
+    /// This function hashes the given password
+    /// </summary>
+    /// <param name="password">The password to be hashed</param>
+    abstract HashPassword : password:string -> string
